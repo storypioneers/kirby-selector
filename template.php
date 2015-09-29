1f72af7c0895c828ba5248c53947024d522c359f
dgdsp <dg@storypioneers.com>
2015-09-30 01:12:08 +0200

    cleanup to match kirby default syntax

--- template.php
+++ template.php
@@ -1,52 +1,51 @@
-
 <input class="[ js-selector-storage ]" type="hidden" name="<?= $field->name() ?>" id="<?= $field->name() ?>" value="<?= implode(',', $field->value()) ?>" />
-
-<div class="input input-with-items">
-    <?php if ($field->files()->count() > 0): ?>
-        <?php foreach ($field->files() as $file): ?>
-            <div id="<?= $field->itemId($file) ?>" class="item item-with-image [ selector-item js-selector-item ]" data-file="<?= $file->filename() ?>" <?= r($field->isInValue($file), 'data-checked="true"') ?> >
-                <div class="item-content">
-                    <?php if ($file->type() == 'image'): ?>
-                        <figure class="item-image">
-                            <a class="item-image-container" href="<?= purl($file, 'show') ?>">
-                                <?= thumb($file, array('width' => 48, 'height' => 48, 'crop' => true)) ?>
-                            </a>
-                        </figure>
-                    <?php else: ?>
-                        <figure class="item-image  item-filetype">
-                            <a class="item-image-container" href="<?= purl($file, 'show') ?>">
-                                <?= strtoupper($file->extension()) ?>
-                            </a>
-                        </figure>
-                    <?php endif ?>
-                    <div class="item-info">
-                        <strong class="item-title">
-                            <a href="<?= purl($file, 'show') ?>">
-                                <?= $file->filename() ?>
-                            </a>
-                        </strong>
-                        <small class="item-meta marginalia">
-                            <?= $file->type() ?> / <?= $file->niceSize() ?>
-                            <?php if ($file->type() == 'image'): ?>
-                                / <?= $file->width() ?> x <?= $file->height() ?>
-                            <?php endif ?>
-                        </small>
-                    </div>
+<?php if ($field->files()->count() > 0): ?>
+  <div class="input input-with-items">
+    <?php foreach ($field->files() as $file): ?>
+        <div id="<?= $field->itemId($file) ?>" class="item item-condensed item-with-image [ selector-item js-selector-item ]" data-file="<?= $file->filename() ?>" <?= r($field->isInValue($file), 'data-checked="true"') ?> >
+            <div class="item-content">
+                <?php if ($file->type() == 'image'): ?>
+                    <figure class="item-image">
+                      <?= thumb($file, array('width' => 48, 'height' => 48, 'crop' => true)) ?>
+                    </figure>
+                <?php else: ?>
+                    <figure class="item-image item-filetype">
+                      <?= strtoupper($file->extension()) ?>
+                    </figure>
+                <?php endif ?>
+                <div class="item-info">
+                    <strong class="item-title">
+                        <?= $file->filename() ?>
+                    </strong>
+                    <small class="item-meta marginalia">
+                        <?= $file->type() ?> / <?= $file->niceSize() ?>
+                        <?php if ($file->type() == 'image'): ?>
+                            / <?= $file->width() ?> x <?= $file->height() ?>
+                        <?php endif ?>
+                    </small>
                 </div>
-                <nav class="item-options">
-                    <ul class="nav nav-bar">
-                        <li>
-                            <a class="btn btn-with-icon [ selector-checkbox js-selector-checkbox ]" href="">
-                                <i class="icon icon-left fa fa-circle-o"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
             </div>
-        <?php endforeach ?>
-    <?php else: ?>
-        <div class="item selector-item-empty">
-            <?php echo l::get('selector.empty') ?>
-        </div>
-    <?php endif ?>
+            <nav class="item-options">
+                <ul class="nav nav-bar">
+                    <li>
+                        <a class="btn btn-with-icon" href="<?= purl($file, 'show') ?>" title="<?= l('pages.show.subpages.edit') ?>">
+                          <i class="icon icon-left fa fa-pencil"></i>
+                          <span><?= l('pages.show.subpages.edit') ?></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="btn btn-with-icon [ selector-checkbox js-selector-checkbox ]" href="#" title="<?= l('pages.show.subpages.add') ?>">
+                          <i class="icon icon-left fa fa-circle-o"></i>
+                          <span><?= l('pages.show.subpages.add') ?></span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+      </div>
+  <?php endforeach ?>
 </div>
+<?php else: ?>
+  <div class="message">
+    <span class="message-content"><?= l::get('selector.empty') ?></span>
+  </div>
+<?php endif ?>
